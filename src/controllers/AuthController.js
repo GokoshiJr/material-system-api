17b8d28a70c99a582b0e9341115dae540a30e1e7
GokoshiJr <julioagonzalez18@gmail.com>
2022-05-01 17:45:22 -0400

    fix: Update AuthController

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Role = require('../models/Role');
 const jwt = require('jsonwebtoken');
 const { secret } = require('../config');
 
@@ -10,14 +11,21 @@ async function signUp(req, res) {
       username,
       email,
       password: await User.encryptPassword(password),
-      roles
     });
+
+    if (roles) { // si envian un rol al crear un usuario, creamos la relacion
+      const foundRoles = await Role.find({ name: {$in: roles} })
+      user.roles = foundRoles.map(role => role._id);
+    } else { // sino tendra el rol user por defecto
+      const role = await Role.findOne({ name: "user" });
+      user.roles = [role._id];
+    }
+
     const savedUser = await user.save();
-    console.log(secret);
     const token = jwt.sign({id: savedUser._id}, secret, {
       expiresIn: 86400 // 24 hours
-    })
-    res.json({ token });
+    });
+    res.status(200).json({ token });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
@@ -26,7 +34,19 @@ async function signUp(req, res) {
 // Login
 async function signIn(req, res) {
   try {
-    res.json("SignIn");
+    const { email, password } = req.body;
+
+    // puebla el feat roles mostrando el objeto entero
+    const userFound = await User.findOne({ email }).populate("roles");
+    if (!userFound) return res.status(400).json({status: "User not found"});
+
+    const matchPassword = await User.comparePassword(password, userFound.password);
+    if (!matchPassword) return res.status(401).json({token: null, status: "Invalid Password"});
+
+    const token = jwt.sign({id: userFound._id}, secret, {
+      expiresIn: 86400 // 24 hours
+    });
+    res.status(200).json({ token })
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
